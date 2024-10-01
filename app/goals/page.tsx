702756e1d5468655ab855bf413b04590cebdede6
Amir Ghari <amir.ghari@metropolia.fi>
2024-10-01 17:19:48 +0300

    page modified

--- app/goals/page.tsx
+++ app/goals/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const page = () => {
-  return <div>My goals</div>
+  return (
+    <div>
+      My goals ha <i class="fa fa-hand-rock-o" aria-hidden="true"></i>
+    </div>
+  )
 }
 
 export default page
