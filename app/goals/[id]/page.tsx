924b5e90252d21705c07720e00e242a9f6206029
Amir Ghari <amir.ghari@metropolia.fi>
2024-10-04 14:49:05 +0300

    Markdown preview is working now

--- app/goals/[id]/page.tsx
+++ app/goals/[id]/page.tsx
@@ -1,6 +1,9 @@
 import prisma from '@/prisma/client'
 import React from 'react'
 import delay from 'delay'
+import CardBadge from '@/app/components/CardBadge'
+import { Card, Flex, Heading, Text } from '@radix-ui/themes'
+import ReactMarkdown from 'react-markdown'
 
 interface Props {
   params: { id: string }
@@ -15,11 +18,16 @@ const GoalAdjustments = async ({ params }: Props) => {
   })
 
   return (
-    <div>
-      <p>Title : {selectedGoal.title}</p>
-      <p>Description : {selectedGoal.description}</p>
-      <p>Created at: {selectedGoal.createdAT.toDateString()}</p>
-      <p>Due Date: {selectedGoal.deadline.toDateString()}</p>
+    <div className="px-4">
+      <Heading>{selectedGoal.title}</Heading>
+      <Flex className="my-4" gap={'4'}>
+        <Text>Created at: {selectedGoal.createdAT.toDateString()}</Text>
+        <CardBadge status={selectedGoal.status} />
+        <Text>Due Date: {selectedGoal.deadline.toDateString()}</Text>
+      </Flex>
+      <Card className="min-w-6 prose">
+        <ReactMarkdown>{selectedGoal.description}</ReactMarkdown>
+      </Card>
     </div>
   )
 }
