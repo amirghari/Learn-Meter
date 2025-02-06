dfd4c962aa709670ba00e100e3d8f75c5bc7d69b
Amir Ghari <amir.ghari@metropolia.fi>
2025-02-06 16:06:42 +0200

    done

--- prisma/migrations/20250206140507_/migration.sql
+++ prisma/migrations/20250206140507_/migration.sql
@@ -60,5 +60,7 @@ CREATE TABLE `verification_tokens` (
     `token` VARCHAR(191) NOT NULL,
     `expires` DATETIME(3) NOT NULL,
 
+    -- Make these two columns the primary key
+    PRIMARY KEY (`identifier`, `token`),
     UNIQUE INDEX `verification_tokens_identifier_token_key`(`identifier`, `token`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
