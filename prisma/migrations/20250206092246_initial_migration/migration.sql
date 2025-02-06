c2a4070584da4875cbbcc62c86ea0463d3ac0712
Amir Ghari <amir.ghari@metropolia.fi>
2025-02-06 11:23:26 +0200

    relation mode is chaned

--- prisma/migrations/20250206092246_initial_migration/migration.sql
+++ prisma/migrations/20250206092246_initial_migration/migration.sql
@@ -1,11 +1,16 @@
-/*
-  Warnings:
+-- CreateTable
+CREATE TABLE `goals` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(255) NOT NULL,
+    `description` TEXT NOT NULL,
+    `deadline` DATE NULL,
+    `status` ENUM('JUST_SET', 'IN_PROGRESS', 'DONE', 'POSTPONED') NOT NULL DEFAULT 'JUST_SET',
+    `createdAT` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `user_email` VARCHAR(191) NOT NULL,
 
-  - The values [ACHIEVED] on the enum `Goal_status` will be removed. If these variants are still used in the database, this will fail.
-
-*/
--- AlterTable
-ALTER TABLE `Goal` MODIFY `status` ENUM('JUST_SET', 'IN_PROGRESS', 'DONE', 'POSTPONED') NOT NULL DEFAULT 'JUST_SET';
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `accounts` (
@@ -41,7 +46,7 @@ CREATE TABLE `sessions` (
 CREATE TABLE `users` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NULL,
-    `email` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NOT NULL,
     `email_verified` DATETIME(3) NULL,
     `image` VARCHAR(191) NULL,
 
@@ -57,9 +62,3 @@ CREATE TABLE `verification_tokens` (
 
     UNIQUE INDEX `verification_tokens_identifier_token_key`(`identifier`, `token`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- AddForeignKey
-ALTER TABLE `accounts` ADD CONSTRAINT `accounts_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `sessions` ADD CONSTRAINT `sessions_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
